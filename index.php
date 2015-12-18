6ab3ab0324249f24e2f78ed34a0e1caafbab0c01
Niklas <niklaswa@users.noreply.github.com>
2015-12-18 18:00:00 +0100

    Initial Commit.

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+error_reporting(0);
+/**
+ * Created by PhpStorm.
+ * User: Niklas
+ * Description: Get YouTube Random Video
+ */
+
+// *** CONFIG *** //
+$YouTubeAPIKey          = '';    // Get one here: https://console.developers.google.com/apis/api/youtube/overview
+$ChannelID              = '';                   // YouTube Channel ID
+$DateOfFirstVideo   = '2000-01-01T00:00:00Z';                       // RFC 3339 formatted date-time value
+$FallbackVideoLink  = 'http://youtube.com/';                        // Fallback viedeo link (e.g. API down)
+// ***        *** //
+
+$publishedBeforeYear = rand(explode('-', $DateOfFirstVideo)[0],date('Y'));
+if($publishedBeforeYear == explode('-', $DateOfFirstVideo)[0]) {
+    $publishedBeforeMonth = str_pad(rand(explode('-', $DateOfFirstVideo)[1],12), 2, '0', STR_PAD_LEFT);
+    $publishedBeforeDay = str_pad(rand(intval(explode('T', explode('-', $DateOfFirstVideo)[2])[0]),31), 2, '0', STR_PAD_LEFT);
+} else {
+    $publishedBeforeMonth = str_pad(rand(1,12), 2, '0', STR_PAD_LEFT);
+    $publishedBeforeDay = str_pad(rand(1,31), 2, '0', STR_PAD_LEFT);
+}
+$publishedBefore = $publishedBeforeYear.'-'.$publishedBeforeMonth.'-'.$publishedBeforeDay;
+$url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId='.$ChannelID.'&maxResults=50&type=video&publishedAfter='.$DateOfFirstVideo.'&publishedBefore='.$publishedBefore.'T00:00:00Z&order=date&fields=etag%2Citems%2Fid&key='.$YouTubeAPIKey;
+$data = json_decode(file_get_contents($url), true);
+
+if(!$data) { // Api down?
+    header("Location: ".$FallbackVideoLink);
+    exit;
+}
+
+$videocount = count($data['items']);
+$randomvideo = rand(0,intval($videocount)-1);
+$videoID = $data['items'][$randomvideo]['id']['videoId'];
+
+if(empty($videoID)) {
+    header("Location: ".$FallbackVideoLink);
+    exit;
+} else {
+    header("Location: http://youtube.com/watch?v=".$videoID);
+    exit;
+}

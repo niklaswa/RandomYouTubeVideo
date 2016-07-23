782f71470eba5dd83f6c64bffd9ff06de184c5a5
Niklas <niklaszockt.mail@gmail.com>
2016-07-23 17:01:44 +0200

    compatibility with old php versions

--- index.php
+++ index.php
@@ -13,13 +13,15 @@ $DateOfFirstVideo   = '2000-01-01T00:00:00Z';                       // RFC 3339 
 $FallbackVideoLink  = 'http://youtube.com/';                        // Fallback video link (e.g. API down)
 // ***        *** //
 
-$publishedBeforeYear = rand(explode('-', $DateOfFirstVideo)[0],date('Y'));
-if($publishedBeforeYear == explode('-', $DateOfFirstVideo)[0]) {
-    $publishedBeforeMonth = str_pad(rand(explode('-', $DateOfFirstVideo)[1],12), 2, '0', STR_PAD_LEFT);
-    $publishedBeforeDay = str_pad(rand(intval(explode('T', explode('-', $DateOfFirstVideo)[2])[0]),31), 2, '0', STR_PAD_LEFT);
+$dateExploded = explode('-', $DateOfFirstVideo);
+$dateExplodedSecond = explode('T', $dateExploded[2]);
+$publishedBeforeYear = rand($dateExploded[0],date('Y'));
+if($publishedBeforeYear == $dateExploded[0]) {
+    $publishedBeforeMonth = str_pad(rand($dateExploded[1], 12), 2, '0', STR_PAD_LEFT);
+    $publishedBeforeDay = str_pad(rand(intval($dateExplodedSecond[0]), 31), 2, '0', STR_PAD_LEFT);
 } else {
-    $publishedBeforeMonth = str_pad(rand(1,12), 2, '0', STR_PAD_LEFT);
-    $publishedBeforeDay = str_pad(rand(1,31), 2, '0', STR_PAD_LEFT);
+    $publishedBeforeMonth = str_pad(rand(1, 12), 2, '0', STR_PAD_LEFT);
+    $publishedBeforeDay = str_pad(rand(1, 31), 2, '0', STR_PAD_LEFT);
 }
 $publishedBefore = $publishedBeforeYear.'-'.$publishedBeforeMonth.'-'.$publishedBeforeDay;
 $url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId='.$ChannelID.'&maxResults=50&type=video&publishedAfter='.$DateOfFirstVideo.'&publishedBefore='.$publishedBefore.'T00:00:00Z&order=date&fields=etag%2Citems%2Fid&key='.$YouTubeAPIKey;
@@ -31,7 +33,7 @@ if(!$data) { // Api down?
 }
 
 $videocount = count($data['items']);
-$randomvideo = rand(0,intval($videocount)-1);
+$randomvideo = rand(0, intval($videocount)-1);
 $videoID = $data['items'][$randomvideo]['id']['videoId'];
 
 if(empty($videoID)) {

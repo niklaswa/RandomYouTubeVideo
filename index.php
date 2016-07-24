1aaa6f0973760bb8544ab4b54250a388383a2bb6
Niklas <niklas.wasserhess@gmail.com>
2016-07-24 16:04:44 +0200

    utf8

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $dateExploded = explode('-', $DateOfFirstVideo);
 $dateExplodedSecond = explode('T', $dateExploded[2]);
 $publishedBeforeYear = rand($dateExploded[0],date('Y'));
 if($debug) {
-    echo 'First video was published in '.$dateExploded[0];
+    echo '<meta charset="utf-8">First video was published in '.$dateExploded[0];
     echo '<br>Randomized year is: '.$publishedBeforeYear;
 }
 if($publishedBeforeYear == $dateExploded[0]) {
